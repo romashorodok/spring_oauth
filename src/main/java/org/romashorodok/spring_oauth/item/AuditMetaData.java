a05603624ef88490c2209a643e5a19815f93176a
romashorodok <108732029+romashorodok@users.noreply.github.com>
2025-04-27 13:42:59 +0300

    feat: add mongo

---
+++ src/main/java/org/romashorodok/spring_oauth/item/AuditMetaData.java
@@ -0,0 +1,26 @@
+package org.romashorodok.spring_oauth.item;
+
+import lombok.Data;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Data
+public class AuditMetaData {
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @CreatedBy
+    private String createdBy;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+
+    @LastModifiedBy
+    private String lastModifiedBy;
+}
